ef94a791c3def74a8079ad63cdb117cc6065ab65
JacobPace <127350081+JacobPace@users.noreply.github.com>
2026-04-05 00:40:53 -0500

    UI Hud with Reticle changing on hover of interactable

--- Proj2/Assets/Scripts/Interactor.cs
+++ Proj2/Assets/Scripts/Interactor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 
 interface IInteractable
@@ -8,23 +9,67 @@ interface IInteractable
 }
 public class Interactor : MonoBehaviour
 {
+    // set in inspector
     public Transform InteractorSource;
     public float InteractRange;
     public LayerMask interactableLayer;
+    private Sprite sprReticleOff;
+    public Sprite sprReticleOn;
+    public Image imgReticle;
 
-    // Update is called once per frame
+    private Color clrDefault;
+    private Color clrHover = Color.cyan;
+
+    private Renderer lastRenderer;
+    [ColorUsage(true, true)]
+    public Color highlightColor = Color.white;
+
+    private void Awake()
+    {
+        sprReticleOff = imgReticle.sprite;
+        clrDefault = imgReticle.color;
+    }
+
     void Update()
     {
-        if (Keyboard.current.eKey.wasPressedThisFrame)
+        imgReticle.sprite = sprReticleOff;
+        imgReticle.color = clrDefault;
+        Ray ray = new(InteractorSource.position, InteractorSource.forward);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, InteractRange, interactableLayer))
         {
-            Ray r = new(InteractorSource.position, InteractorSource.forward);
-            if(Physics.Raycast(r, out RaycastHit hitInfo, InteractRange, interactableLayer))
+            imgReticle.sprite = sprReticleOn;
+            imgReticle.color = clrHover;
+            if (hitInfo.collider.TryGetComponent(out Renderer currentRenderer))
             {
-                if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObject))
+                if (currentRenderer != lastRenderer)
                 {
-                    interactObject.Interact();
+                    ResetOutline();
+                    lastRenderer = currentRenderer;
+                    lastRenderer.material.SetColor("_EmissionColor", highlightColor);
+                }
+
+                if (Keyboard.current.eKey.wasPressedThisFrame)
+                {
+                    if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObject))
+                    {
+                        interactObject.Interact();
+                    }
+
                 }
             }
+        }
+        else
+        {
+            ResetOutline();
+        }
+    }
+
+    void ResetOutline()
+    {
+        if (lastRenderer != null)
+        {
+            lastRenderer.material.SetColor("_EmissionColor", Color.black);
+            lastRenderer = null;
         }
     }
 }
