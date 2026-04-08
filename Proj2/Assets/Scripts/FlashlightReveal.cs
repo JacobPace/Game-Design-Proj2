8dfe9d65ce4d7ae4259804ac603e8763f4f4c9aa
Leinad42 <153773009+Leinad42@users.noreply.github.com>
2026-04-08 02:31:22 -0500

    Added hidden number mechanic for flashlight

---
+++ Proj2/Assets/Scripts/FlashlightReveal.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class FlashlightReveal : MonoBehaviour
+{
+    public GameObject hiddenNumber;
+    public NewFlashlight flashlight;
+    public Transform flashlightTransform;
+    public float revealDistance = 5f;
+    public LayerMask revealLayer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (hiddenNumber != null)
+        {
+            hiddenNumber.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("FlashlightReveal running on " + gameObject.name);
+
+        if (hiddenNumber == null || flashlight == null || flashlightTransform == null)
+            return;
+
+        bool shouldShow = false;
+
+        if (flashlight.hasFlashlight && flashlight.isON)
+        {
+            Ray ray = new Ray(flashlightTransform.position, flashlightTransform.forward);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, revealDistance))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    shouldShow = true;
+                }
+            }
+        }
+
+        hiddenNumber.SetActive(shouldShow);
+    }
+}
