bbafe46894523e15680acf9376c4f905d2254d18
JacobPace <127350081+JacobPace@users.noreply.github.com>
2026-04-08 20:58:23 -0500

    Combined Candle Puzzle into Library Scene

--- Proj2/Assets/Scripts/MainMenu.cs
+++ Proj2/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
     public GameObject infoScreen;
     public void StartGame()
     {
-        SceneManager.LoadScene("CandleTestScene");
+        SceneManager.LoadScene("LibraryTest");
     }
 
     public void InfoScreen()
